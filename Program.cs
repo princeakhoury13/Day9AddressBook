d119ec4f415ee077b2cba1e4bb247c0797c0fe24
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-20 17:36:16 +0530

    added: Master greeting message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day9AddressBook
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book\r\nProgram in\r\nAddressBookMain class on\r\n\r\nSTART Master Branch");
+        }
+    }
+}
