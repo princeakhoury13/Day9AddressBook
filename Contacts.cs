00977156653b5ca33b777f38ff6e8e034e40a146
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-20 17:58:15 +0530

    added: Structure for contacts

---
+++ Contacts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9AddressBook
+{
+    public class Contacts
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string country { get; set; }
+        public string phoneNumber { get; set; }
+        public string email { get; set; }
+
+
+        public Contacts(string firstName, string lastName, string country, string phoneNumber, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.country = country;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+
+
+    }
+}
