de3b734f576e142a5bcb019b3903bca06161a190
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-20 18:41:35 +0530

    added: Deleting an existing contact

--- Program.cs
+++ Program.cs
@@ -6,11 +6,16 @@
         {
             AddressBook myAddressBook = new AddressBook();
             Contacts newContact = new Contacts("Prince", "Praveen", "INDIA", "8369319177", "prince.p@example.com");
+            Contacts newContact1 = new Contacts("Piyush", "Akhoury", "INDIA", "8879393114", "piyush.p@example.com");
+            Contacts newContact2 = new Contacts("Tony", "Stark", "USA", "8885550000", "tony.stark@tech.com");
             myAddressBook.AddContact(newContact);
+            myAddressBook.AddContact(newContact1);
+            myAddressBook.AddContact(newContact2);
             myAddressBook.PrintContacts();
 
-            myAddressBook.EditContacts("Prince", "Praveen", "Prince", "Akhoury", "INDIA", "8369319188", "prince13.p@example.com");
+            myAddressBook.DeleteContact("Piyush", "Akhoury");
             myAddressBook.PrintContacts();
+
 
         }
     }
