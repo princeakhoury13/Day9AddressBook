6e04ef57c70af5dbfe8ec6c9ffc029dae57e5d70
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-20 18:03:54 +0530

    added: Taking new contact details from Console

--- Program.cs
+++ Program.cs
@@ -5,10 +5,27 @@
         public static void Main(string[] args)
         {
             AddressBook myAddressBook = new AddressBook();
-            Contacts newContact = new Contacts("Prince", "Praveen", "INDIA", "8369319177", "prince.p@example.com");
+
+
+            Console.WriteLine("Add new contact:");
+            Console.Write("Enter first name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter last name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Enter country: ");
+            string country = Console.ReadLine();
+            Console.Write("Enter phone number: ");
+            string phoneNumber = Console.ReadLine();
+            Console.Write("Enter email: ");
+            string email = Console.ReadLine();
+
+            Contacts newContact = new Contacts(firstName, lastName, country, phoneNumber, email);
 
             myAddressBook.AddContact(newContact);
+            Console.WriteLine("Contact Details: ");
             myAddressBook.PrintContacts();
+
+
         }
     }
 }
