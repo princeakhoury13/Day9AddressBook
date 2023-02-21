aa53e62eef2dce27a7aeb3a4b0c43ae48b2f3655
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-21 11:34:01 +0530

    added: Create multiple contacts

--- Program.cs
+++ Program.cs
@@ -4,17 +4,20 @@
     {
         public static void Main(string[] args)
         {
-            AddressBook myAddressBook = new AddressBook();
-            Contacts newContact = new Contacts("Prince", "Praveen", "INDIA", "8369319177", "prince.p@example.com");
-            Contacts newContact1 = new Contacts("Piyush", "Akhoury", "INDIA", "8879393114", "piyush.p@example.com");
-            Contacts newContact2 = new Contacts("Tony", "Stark", "USA", "8885550000", "tony.stark@tech.com");
-            myAddressBook.AddContact(newContact);
-            myAddressBook.AddContact(newContact1);
-            myAddressBook.AddContact(newContact2);
-            myAddressBook.PrintContacts();
 
-            myAddressBook.DeleteContact("Piyush", "Akhoury");
+            AddressBook myAddressBook = new AddressBook();
+
+            Contacts newContact = new Contacts("Prince", "Praveen", "INDIA", "8369319177", "prince.p@example.com");
+            myAddressBook.AddContact(newContact);
+
+            Contacts newContact1 = new Contacts("Piyush", "Akhoury", "INDIA", "8879393114", "piyush.p@example.com");
+            myAddressBook.AddContact(newContact1);
+
+            Contacts newContact2 = new Contacts("Tony", "Stark", "USA", "8885550000", "tony.stark@tech.com");
+            myAddressBook.AddContact(newContact2);
+
             myAddressBook.PrintContacts();
+            
 
 
         }
